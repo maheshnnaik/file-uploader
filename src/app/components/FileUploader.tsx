6f65c595e4e619d7635c9b718615731cd3aba484
maheshnnaik <maheshnaik2747@gmail.com>
2024-11-15 16:26:42 +0530

    create component for upload in progress

--- src/app/components/FileUploader.tsx
+++ src/app/components/FileUploader.tsx
@@ -2,12 +2,19 @@
 
 import Image from "next/image"
 import { useState } from "react";
+import { Loading } from "./Loading";
 
 export const FileUplolader = () => {
     const [isDragging, setIsDragging] = useState(false);
+    const [isUploading, setIsUploading] = useState(false);
     const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        setIsUploading(true)
         if (event.target.files && event.target.files.length > 0) {
             const newFile = event.target.files[0]
+            setIsUploading(false)
+            setIsUploadComplete(true)
+            setFile(newFile);
             console.log(newFile);
         }
     };
@@ -26,6 +33,14 @@ export const FileUplolader = () => {
         event.preventDefault();
         setIsDragging(false)
     }
+
+    const renderContent = () => {
+        if (isUploading) {
+            return <UploadInProgress />
+        }
+        return <FileUpload handleFileSelect={handleFileSelect} />
+    }
+
     return (
 
         <div 
@@ -35,19 +50,27 @@ export const FileUplolader = () => {
             onDrop={handleDrop}
         >
             <div className="flex flex-col items-center gap-2">
-                <Image
-                    src={'/cloud-arrow-up.svg'}
-                    width={24}
-                    height={24}
-                    alt="upload_file"
-                />
-                <span className="text-[#4B4B4B] font-semibold text-[14px]">Drop files here to upload</span>
-                <div className="relative bg-[#E8E8E8] py-2.5 px-3.5 rounded-3xl cursor-pointer">
-                    <label className="inline-block text-black font-semibold text-[14px]" htmlFor="fileInput">
-                        Browse Files
-                        <input type="file" className="opacity-0 w-full h-full absolute left-0 top-0 cursor-pointer" onChange={handleFileSelect} />
-                    </label>
+                {
+                    renderContent()
+                }
+            </div>
+        </div>
+    )
+}
 
+const UploadInProgress = () => {
+    return (
+        <>
+            <Loading />
+            <span className="text-[#4B4B4B] font-semibold text-[14px]">Uploading File</span>
+            <div className="relative bg-[#E8E8E8] py-2.5 px-3.5 rounded-3xl cursor-pointer">
+                <label className="inline-block text-black font-semibold text-[14px]" htmlFor="fileInput">
+                    Cancel
+                </label>
+            </div>
+        </>
+    )
+}
                 </div>
             </div>
         </div>
