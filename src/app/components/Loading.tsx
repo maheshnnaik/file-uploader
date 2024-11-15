baf2d66b520bf4832bb936f340f11f8792ab0df8
maheshnnaik <maheshnaik2747@gmail.com>
2024-11-15 16:24:49 +0530

    Create Loading.tsx

---
+++ src/app/components/Loading.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+    return (
+        <div className="h-6 w-6 flex items-center justify-center">
+            <div className="h-6 w-6 animate-spin rounded-full border-b-2 border-[#276EF1]"></div>
+        </div>
+    );
+};
