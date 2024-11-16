6e00797234a0cc1907aa5e7501f066d0cfcd3547
maheshnnaik <maheshnaik2747@gmail.com>
2024-11-16 17:12:21 +0530

    add open sans font

--- src/app/utils/font.ts
+++ src/app/utils/font.ts
@@ -1,6 +1,12 @@
-import { Noto_Sans_JP } from "next/font/google";
+import { Noto_Sans_JP, Open_Sans } from "next/font/google";
 
 export const notoSansJP = Noto_Sans_JP({
+    subsets: ["latin"],
+    weight: ['400', '600', '800'],
+    preload: true
+});
+
+export const openSans = Open_Sans({
     subsets: ["latin"],
     weight: ['400', '600', '800'],
     preload: true
